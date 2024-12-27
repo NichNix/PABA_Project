5fb16ad362541e0a254ad9401e7ff97e179adae2
HedgeHawk808 <kenncahyaputra142@gmail.com>
2024-12-27 12:36:04 +0700

    user profile done

--- app/src/main/java/paba/project/berita_online/database/AppDatabase.kt
+++ app/src/main/java/paba/project/berita_online/database/AppDatabase.kt
@@ -18,7 +18,7 @@ abstract class AppDatabase : RoomDatabase() {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "app_database"
+                    "user-database"
                 )
                     .fallbackToDestructiveMigration()
                     .build()
